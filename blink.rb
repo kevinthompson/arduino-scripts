ffa247d381d14fa16c040392aa787775c0a33a19
Kevin Thompson <kevin@kevinthompson.info>
2013-01-06 09:49:09 -0800

    Update light blinking speed to be controlled by a sensor input.

--- blink.rb
+++ blink.rb
@@ -1,11 +1,16 @@
 require 'rubygems'
 require 'dino'
 
-board = Dino::Board.new(Dino::TxRx.new)
-led   = Dino::Components::Led.new(pin: 13, board: board)
+board  = Dino::Board.new(Dino::TxRx.new)
+sensor = Dino::Components::Sensor.new(pin: 'A0', board: board)
+led    = Dino::Components::Led.new(pin: 13, board: board)
 
-[:on, :off].cycle do |switch|
-  led.send(switch)
-  puts switch
-  sleep 1
+on_data = Proc.new do |data|
+  led.off
+  sleep data.to_f/1000
+  led.on
 end
+
+sensor.when_data_received on_data
+
+sleep
